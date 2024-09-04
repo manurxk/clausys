9bf6bc776cb709edb7a3d79dd575f94180981b39
gallopelado <juanftp100@gmail.com>
2024-09-04 19:34:51 -0400

    feat: trabajando con herencia de plantillas

--- app/rutas/referenciales/ciudad/ciudad_routes.py
+++ app/rutas/referenciales/ciudad/ciudad_routes.py
@@ -5,3 +5,7 @@ ciumod = Blueprint('ciudad', __name__, template_folder='templates')
 @ciumod.route('/ciudad-index')
 def ciudadIndex():
     return render_template('ciudad-index.html')
+
+@ciumod.route('/ciudad-agregar')
+def ciudadAgregar():
+    return render_template('ciudad-agregar.html')
