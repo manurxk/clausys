1cbf6117e1ff6ab391875a4544f0e36d1c72eb8c
gallopelado <juanftp100@gmail.com>
2024-10-16 20:08:51 -0300

    tmp: agregar, defincion

--- app/dao/gestionarcompras/registrarpedidocompras/PedidoDeComprasDao.py
+++ app/dao/gestionarcompras/registrarpedidocompras/PedidoDeComprasDao.py
@@ -4,6 +4,25 @@ from app.dao.gestionarcompras.registrarpedidocompras.dto import PedidoDeComprasD
 
 class PedidoDeComprasDao:
     
+    def obtenerTodosLosPedidos(self):
+        todoslospedidosSQL = """
+        SELECT
+            pdc.id_pedido_compra
+            , pdc.id_empleado, p.nombres, p.apellidos,
+            , pdc.id_sucursal
+            , pdc.id_epc, edpc.descripcion,
+            , pdc.fecha_pedido
+            , pdc.id_deposito
+        FROM
+            public.pedido_de_compra AS pdc
+        LEFT JOIN empleados e
+            ON e.id_empleado = pdc.id_empleado
+        LEFT JOIN personas p
+            ON p.id_persona = e.id_empleado
+        LEFT JOIN estado_de_pedido_compras edpc
+        ON pdc.id_epc = edpc.id_epc
+        """
+
     # agregar
     def agregar(self, pedidoDto: PedidoDeComprasDto):
         insertPedidoCompraCabecera = """
@@ -22,15 +41,32 @@ class PedidoDeComprasDao:
         # objeto conexion
         conexion = Conexion()
         con = conexion.getConexion()
+        con.autocommit = False
         cur = con.cursor()
         try:
-            parametros = ()
+            ## Insertando la cabecera
+            # (id_empleado, id_sucursal, id_epc, fecha_pedido, id_deposito)
+            parametros = (pedidoDto.id_empleado, pedidoDto.id_sucursal, \
+                pedidoDto.id_epc, pedidoDto.fecha_pedido, pedidoDto.id_deposito,)
             cur.execute(insertPedidoCompraCabecera, parametros)
+            id_pedido_compra = cur.fetchone()[0]
+
+            ## Insertando el detalle del pedido
+            if len(pedidoDto.detallePedido) > 0:
+                for pedido in pedidoDto.detallePedido:
+                    # (id_pedido_compra, id_producto, cantidad)
+                    parametrosdetalle = (id_pedido_compra, pedido.id_producto, pedido.cantidad,)
+                    cur.execute(insertDetalleCompra, parametrosdetalle)
+
+            # el ricolin, commit
+            con.commit()
 
         except Exception as e:
             app.logger.error(f"Error a agregar un nuevo pedido: {str(e)}")
+            con.rollback()
             return False
         finally:
+            con.autocommit = True
             cur.close()
             con.close()
 
--- app/dao/gestionarcompras/registrarpedidocompras/dto/PedidoDeComprasDto.py
+++ app/dao/gestionarcompras/registrarpedidocompras/dto/PedidoDeComprasDto.py
@@ -1,12 +1,14 @@
+from typing import List
+from app.dao.gestionarcompras.registrarpedidocompras.dto.PedidoDeCompraDetalleDto import PedidoDeCompraDetalleDto
+
 class PedidoDeComprasDto:
     
-    def __init__(self, id, id_empleado, id_usuario, id_sucursal, \
-        id_estado, fechapedido, id_deposito, pedidoDeCompraDetalleDto):
-        self.id = id
+    def __init__(self, id_pedido_compra: int, id_empleado: int, id_sucursal: int, \
+        id_epc: int, fecha_pedido, id_deposito: int, detallePedido: List[PedidoDeCompraDetalleDto]):
+        self.id_pedido_compra = id_pedido_compra
         self.id_empleado = id_empleado
-        self.id_usuario = id_usuario
         self.id_sucursal = id_sucursal
-        self.id_estado = id_estado
-        self.fechapedido = fechapedido
+        self.id_epc = id_epc
+        self.fecha_pedido = fecha_pedido
         self.id_deposito = id_deposito
-        self.pedidoDeCompraDetalleDto = pedidoDeCompraDetalleDto
+        self.detallePedido = detallePedido
