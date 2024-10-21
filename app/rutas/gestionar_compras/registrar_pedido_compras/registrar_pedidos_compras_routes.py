a43c093d02796f411e76a1fc3baff5d7fb106a2a
gallopelado <juanftp100@gmail.com>
2024-10-21 19:52:20 -0300

    tmp: construyendo formulario agregar

--- app/rutas/gestionar_compras/registrar_pedido_compras/registrar_pedidos_compras_routes.py
+++ app/rutas/gestionar_compras/registrar_pedido_compras/registrar_pedidos_compras_routes.py
@@ -5,3 +5,7 @@ pdcmod = Blueprint('pdcmod', __name__, template_folder='templates')
 @pdcmod.route('/pedidos-index')
 def pedidos_index():
     return render_template('pedidos-index.html')
+
+@pdcmod.route('/pedidos-agregar')
+def pedidos_agregar():
+    return render_template('pedidos-agregar.html')
